216c0367d04a83268d4aad30a9435d50685699f4
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-22 15:03:08 +0200

    create

---
+++ fd-test.session.sql
@@ -0,0 +1,31 @@
+CREATE TABLE books(
+   name varchar(300),
+   author varchar(300),
+   type varchar(150),
+   pages int,
+   year date,
+   publisher varchar(256)
+);
+
+DROP TABLE books;
+
+/*
+
+створіть таблицю users
+
+В кожного юзера має бути:
+- firstName
+- lastName
+- email
+- birthday
+- gender 
+
+*/
+
+CREATE TABLE users(
+    first_name varchar(256),
+    last_name varchar(256),
+    email varchar(300),
+    birthday date,
+    gender varchar(100)
+);
