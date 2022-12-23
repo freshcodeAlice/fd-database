095c7c409a6fbe93880ba6eeea0a0c8ff499b84b
freshcodeAlice <freshjs.ofte@gmail.com>
2022-12-23 14:54:42 +0200

    Alter

--- fd-test.session.sql
+++ fd-test.session.sql
@@ -25,16 +25,45 @@ DROP TABLE books;
 DROP TABLE users;
 
 CREATE TABLE users(
+    id serial PRIMARY KEY,
     first_name varchar(256) NOT NULL CHECK (first_name != ''),
     last_name varchar(256) NOT NULL CHECK (last_name != ''),
     email varchar(300) NOT NULL UNIQUE CHECK (last_name != ''),
     birthday date CHECK (birthday < current_date),
-    gender varchar(100) CHECK (last_name != ''),
-    CONSTRAINT name_pair UNIQUE(first_name, last_name)
+    gender varchar(100) CHECK (last_name != '')
 );
 
-INSERT INTO users VALUES 
-('Clark', 'Kent', 'super2@man.com', '2025-09-09', 'male');
+ALTER TABLE users
+ADD COLUMN height numeric(3,2);
+
+ALTER TABLE users
+ADD CONSTRAINT "too_high_user" CHECK (height < 4.0);
+
+
+/*
+Додати юзерам нову колонку - вагу. Вага не має бути менше 0
+
+Додати перевірку, що користувач народився не раніше 1990року.
+
+*/
+
+ALTER TABLE users
+ADD COLUMN weight numeric(5, 2) CHECK (weight > 0);
+
+ALTER TABLE users
+ADD CONSTRAINT "too_early_birthday" CHECK (birthday > '1990-01-01');
+
+
+//table_field_check
+//table_field_pkey
+
+ALTER TABLE users
+DROP CONSTRAINT "too_high_user";
+
+INSERT INTO users (first_name, last_name, email, birthday, gender) VALUES 
+('Clark', 'Kent', 'super@man.com', '2020-09-09', 'male'),
+('Iron', 'Man', 'tony@mail.com', '1990-02-02', 'male'),
+('Spider', 'MAn', 'peter@parker.com', '1992-02-02', 'male');
 
 INSERT INTO users VALUES
 (' ', '', 'tonestark2@com', '1892-02-02', 'male'),
@@ -42,8 +71,8 @@ INSERT INTO users VALUES
 ('She', '', 'tonestark3@com', '1992-02-02', 'female'),
 (' ', ' ', ' ', NULL,NULL );
 
-INSERT INTO users VALUES 
-('Clark', NULL, 4, '1990-2-2', 'male');
+INSERT INTO users  (first_name, last_name, email, birthday, gender, height) VALUES 
+('Clark', 'MArk ', 'tre@ls.s', '1990-2-2', 'male', 2.0);
 
 /*
 Створити таблицю messages
@@ -70,3 +99,6 @@ INSERT INTO messages VALUES
 
 INSERT INTO messages (author, body) VALUES
 ('test user', 'hellwwwwwwwwwlo text');
+
+
+
