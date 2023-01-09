74fbe18d21578210c7c442a52c9b9e564c1d6692
freshcodeAlice <freshjs.ofte@gmail.com>
2023-01-09 17:03:49 +0200

    IN, ALL, EXISTS, ANY/SOME

--- configs/db.js
+++ configs/db.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '718',
-    database: 'fd_test'
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: '718',
+  database: 'fd_test'
 }
--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const {client, User, Phone, Order} = require('./models/index');
-const {getUsers} = require('./api/index');
-const {generatePhones} = require('./utils/generate');
+const { client, User, Phone, Order } = require('./models/index')
+const { getUsers } = require('./api/index')
+const { generatePhones } = require('./utils/generate')
 
+async function start () {
+  await client.connect()
 
-async function start() {
-    await client.connect();
+  const usersArray = await getUsers()
 
-    const usersArray = await getUsers();
-
-//    const {rows} = await User.bulkCreate(usersArray);
-const {rows: users} = await User.findAll();
-    // const phones = await Phone.bulkCreate(generatePhones(100)); 
-    const phones = await Phone.findAll();
-    const orders = await Order.bulkCreate(users, phones);
-   await client.end();
+  //  const { rows } = await User.bulkCreate(usersArray)
+  const { rows: users } = await User.findAll()
+  const phones = await Phone.bulkCreate(generatePhones(100))
+  //    const phones = await Phone.findAll();
+  const orders = await Order.bulkCreate(users, phones)
+  await client.end()
 }
 
-
-start();
+start()
